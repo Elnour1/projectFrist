80562c1945a528bfc3b4faf6bb492cef5c15f09e
Nour Elsayed <a_nah_123@hotmail.com>
2026-04-07 01:59:26 +0200

    feat: create navbar component

--- src/feature/NavBar.jsx
+++ src/feature/NavBar.jsx
@@ -1,6 +1,6 @@
 export default function NavBar() {
     return (
-        <nav className="navbar"> 
+        <nav className="navbar bg-gray-800 text-white p-4"> 
                 <h1 className="navbar-title">My Website</h1>   
         </nav>
     );
