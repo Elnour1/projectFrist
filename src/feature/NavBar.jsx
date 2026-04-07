a78dfee1e3fbba1fa19a080f20c5ffa5575f93f8
Nour Elsayed <a_nah_123@hotmail.com>
2026-04-07 02:23:46 +0200

    feat: create navbar component

--- src/feature/NavBar.jsx
+++ src/feature/NavBar.jsx
@@ -1,7 +1,13 @@
+import '../App.css';
 export default function NavBar() {
     return (
-        <nav className="navbar bg-gray-800 text-white p-4"> 
-                <h1 className="navbar-title">My Website</h1>   
+        <nav className="navbar"> 
+                <h1 className="navbar-title">My Website</h1>
+                    <ul className="navbar-links"> 
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/contact">Contact</a></li>  
+                    </ul>
         </nav>
     );
 }
